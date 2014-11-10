50c0f4f0128c6aacc6ad013407083f69e10f0942
traivan98 <traivan98@gmail.com>
2014-11-09 22:09:57 -0800

    all files are up :happy

---
+++ js/active.js
@@ -0,0 +1,6 @@
+// you can use document ready function for activing your plugin .
+$( document ).ready(function() {
+	// your plugin activation here
+	// using headingAni ID we are activeing heading animation here.
+	$('#headingAni').addClass('animated bounce');
+});
